d854a3326daa15ef182d1407c51e5890bae2689d
diegomenezeslopes <diegolopes.academico@gmail.com>
2019-11-29 19:59:31 -0400

    New functions

--- lib/flutter_player_plugin.dart
+++ lib/flutter_player_plugin.dart
@@ -10,4 +10,24 @@ class FlutterPlayerPlugin {
     final String version = await _channel.invokeMethod('getPlatformVersion');
     return version;
   }
+
+  static Future<void> get loadFile async {
+    await _channel.invokeMethod('loadFile');
+  }
+
+  static Future<void> get play async {
+    await _channel.invokeMethod('play');
+  }
+
+  static Future<void> get pause async {
+    await _channel.invokeMethod('pause');
+  }
+
+  static Future<void> get stop async {
+    await _channel.invokeMethod('stop');
+  }
+
+  static Future<void> get playLoop async {
+    await _channel.invokeMethod('play_loop');
+  }
 }
